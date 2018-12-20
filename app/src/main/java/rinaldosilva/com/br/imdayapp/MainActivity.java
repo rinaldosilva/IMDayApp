c727281ed21ae476da501cc854d63d4310c70ec5
rinaldo.silva@hotmail.com <ri220599>
2018-12-20 15:41:22 -0300

    # Commit inicial do projeto

---
+++ app/src/main/java/rinaldosilva/com/br/imdayapp/MainActivity.java
@@ -0,0 +1,13 @@
+package rinaldosilva.com.br.imdayapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
